48131f4cf09dfa95b45c1374ea8e00e3e8151955
Olson <976308589@qq.com>
2019-06-11 09:46:36 +0800

    爬虫成功

--- XC.py
+++ XC.py
@@ -6,6 +6,8 @@ import requests as req
 from path import Path
 from selenium import webdriver
 
+WEB = True
+
 
 class Ope:
     def __init__(self):
@@ -17,22 +19,25 @@ class Ope:
     def parser_res(res):
         if '验证访问' in res.text:
             print('访问次数过多')
-            driver = webdriver.Chrome()
-            driver.get(res.url)
-            sleep(10)
+            if WEB:
+                driver = webdriver.Chrome()
+                driver.get(res.url)
+                sleep(10)
+            else:
+                sleep(30)
             return False
         return True
 
     def get_city_code(self, city):
-        res = req.get(self.city_code, params={'keyword': city, 'from': 'domestic'})
+        res = req.get(self.city_code, params={'keyword': city, 'from': 'domestic'}, timeout=5)
         while not self.parser_res(res):
-            res = req.get(self.city_code, params={'keyword': city, 'from': 'domestic'})
+            res = req.get(self.city_code, params={'keyword': city, 'from': 'domestic'}, timeout=5)
         return re.findall(f'"key":"{city}","data":"@[a-z]+\'*[a-z]+\|{city}\|(\d+)\|', res.text, re.I)
 
     def get_sub_city_code(self, city_code):
-        res = req.get(self.sub_city_code, params={'cityid': city_code})
+        res = req.get(self.sub_city_code, params={'cityid': city_code}, timeout=5)
         while not self.parser_res(res):
-            res = req.get(self.sub_city_code, params={'cityid': city_code})
+            res = req.get(self.sub_city_code, params={'cityid': city_code}, timeout=5)
         res = res.text
         if 'cQuery.jsonpResponse={};cQuery.jsonpResponse.suggestion=null' in res:
             return []
@@ -141,31 +146,35 @@ class Ope:
                 res['cityId'] = sub_city_code['id']
         return res
 
-    def _get_all_hotel_num(self, city_code, sub_city_code):
-        res = self._get_pars(city_code, sub_city_code)
-        res = req.post(self.hotel_num, res)
-        while not self.parser_res(res):
-            res = req.post(self.hotel_num, res)
-        return res.json()['hotelAmount']
-
-    def _get_hotel_num_by_price(self, city_code, sub_city_code):
-        res = self._get_pars(city_code, sub_city_code)
-        res['price'] = 'v0v200'
-        res = req.post(self.hotel_num, res)
-        while not self.parser_res(res):
-            res = req.post(self.hotel_num, res)
-        return res.json()['hotelAmount']
-
-    def _get_hotel_num_by_star(self, city_code, sub_city_code):
-        res = self._get_pars(city_code, sub_city_code)
-        res['star'] = '4,5'
-        res = req.post(self.hotel_num, res)
-        while not self.parser_res(res):
-            res = req.post(self.hotel_num, res)
+    @staticmethod
+    def _parser_num(res):
         try:
             return res.json()['hotelAmount']
         except:
             return re.findall('"hotelAmount":(\d*),', res.text)[0]
+
+    def _get_all_hotel_num(self, city_code, sub_city_code):
+        res = self._get_pars(city_code, sub_city_code)
+        res = req.post(self.hotel_num, res, timeout=5)
+        while not self.parser_res(res):
+            res = req.post(self.hotel_num, res, timeout=5)
+        return self._parser_num(res)
+
+    def _get_hotel_num_by_price(self, city_code, sub_city_code):
+        res = self._get_pars(city_code, sub_city_code)
+        res['price'] = 'v0v200'
+        res = req.post(self.hotel_num, res, timeout=5)
+        while not self.parser_res(res):
+            res = req.post(self.hotel_num, res, timeout=5)
+        return self._parser_num(res)
+
+    def _get_hotel_num_by_star(self, city_code, sub_city_code):
+        res = self._get_pars(city_code, sub_city_code)
+        res['star'] = '4,5'
+        res = req.post(self.hotel_num, res, timeout=5)
+        while not self.parser_res(res):
+            res = req.post(self.hotel_num, res)
+        return self._parser_num(res)
 
     def get_hotel_num(self, city_code, sub_city_code):
         all_num = self._get_all_hotel_num(city_code, sub_city_code)
@@ -201,21 +210,24 @@ class XC(Ope):
     def run(self):
         loads = self.get_loads()
         for city in self.get_all_city:
-            city_code = self.get_city_code(city)
-            if not city_code:
-                continue
-            sub_city_code = self.get_sub_city_code(city_code)
-            if sub_city_code:
-                for sub_city in sub_city_code:
-                    if f'{city},{sub_city["name"]}' not in loads:
-                        num = self.get_hotel_num(city_code, sub_city)
-                        res = [city, sub_city["name"]] + list(num)
+            try:
+                city_code = self.get_city_code(city)
+                if not city_code:
+                    continue
+                sub_city_code = self.get_sub_city_code(city_code)
+                if sub_city_code:
+                    for sub_city in sub_city_code:
+                        if f'{city},{sub_city["name"]}' not in loads:
+                            num = self.get_hotel_num(city_code, sub_city)
+                            res = [city, sub_city["name"]] + list(num)
+                            self.dump(res)
+                else:
+                    if f'{city},{city}' not in loads:
+                        num = self.get_hotel_num(city_code, city)
+                        res = [city, city] + list(num)
                         self.dump(res)
-            else:
-                if f'{city},{city}' not in loads:
-                    num = self.get_hotel_num(city_code, city)
-                    res = [city, city] + list(num)
-                    self.dump(res)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
